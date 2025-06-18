fbfeb25975f368c5ced96d4d155fac2e1aa14161
Azizul Hakim <hakim861202@gmail.com>
2025-06-18 18:21:46 +0200

    Update Histogram2.R

--- RDSML-Day-14 👉 Histogram in R (Part-2)/Histogram2.R
+++ RDSML-Day-14 👉 Histogram in R (Part-2)/Histogram2.R
@@ -13,5 +13,36 @@ ggplot(dataset,aes(x=weight))+
 ggplot(dataset,aes(x=weight, fill=sex))+
 geom_histogram(color = "white", alpha=0.5, position ="identity")
 
+#We can change the position adjustment to use for overlapping points on the layer
+
+#possible values for the argument position are
+#"identity", "stack", "dodge". default value is "stack"
+
+#interleaved histogram
+ggplot(dataset,aes(x=weight, fill=sex))+
+  geom_histogram(color = "white", alpha=0.5, position ="dodge")
+
+ggplot(dataset,aes(x=weight, fill=sex))+
+  geom_histogram(color = "white", alpha=0.5, position ="stack")
+
+ggplot(dataset,aes(x=weight, fill=sex))+
+  geom_histogram(color = "white", alpha=0.5, position ="dodge")+
+theme(legend.position = "top")
+
+#The plyr package is used to calculate the mean weight of each group
+
+#install.packages("plyr")
+library(plyr)
+group_means =ddply(dataset, "sex", summarise, grp.mean= mean(weight))
+
+group_means
+
+#adding mean line
+histo2plot= ggplot(dataset,aes(x=weight, fill=sex))+
+  geom_histogram(color = "white", alpha=0.5, position ="dodge")+
+  geom_vline(data= group_means, aes(xintercept = grp.mean, color=sex), linetype ="dashed")+
+  theme(legend.position = "top")
+  
+histo2plot
 
 
