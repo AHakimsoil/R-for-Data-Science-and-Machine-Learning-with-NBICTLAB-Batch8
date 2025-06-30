7b009f80f0f17312821672eeb07de5663987b286
Azizul Hakim <hakim861202@gmail.com>
2025-06-30 18:34:35 +0200

    Create Error bar.R

---
+++ RDSML-Day-17 👉 Error Bars in R (Part-1)/Error bar.R
@@ -0,0 +1,13 @@
+#importing the data set
+tooth_data= ToothGrowth
+
+#creating a function for summarizing the data
+#based on grouping variables
+
+sum =function(a, b, c, d){
+  result=a+b+c+d
+  return(result)
+  
+}
+
+sum(23, 25, 89, 87)
