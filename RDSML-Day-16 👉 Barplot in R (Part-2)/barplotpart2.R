de9942a1a6bb57cc61a8c24d016a82f284849139
Azizul Hakim <hakim861202@gmail.com>
2025-06-30 17:40:50 +0200

    Update barplotpart2.R

--- RDSML-Day-16 👉 Barplot in R (Part-2)/barplotpart2.R
+++ RDSML-Day-16 👉 Barplot in R (Part-2)/barplotpart2.R
@@ -1,56 +1,88 @@
-
-dataset= data.frame(dose= c("D0.5", "D1", "D2"), len= c(4.2, 10, 29.5),
-                    gsg = c(45, 77,7))
+# Creating a data frame
+dataset = data.frame(dose = c("D0.5", "D1", "D2"), 
+                     len = c(4.2, 10, 29.5))
 
 library(ggplot2)
 
-#Basic barplot
-ggplot(data=dataset, aes(x=dose, y=len))+
+# Basic barplot
+ggplot(data = dataset, aes(x = dose, y = len)) + 
   geom_bar(stat = "identity")
 
 # Horizontal barplot
-ggplot(data=dataset, aes(x=dose, y=len))+
-  geom_bar(stat = "identity")+
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity") +
   coord_flip()
 
-#Changing the width of bars
-ggplot(data=dataset, aes(x=dose, y=len))+
-  geom_bar(stat = "identity", width = 0.75)
+# Changing the width of bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", width = .5)
 
-#Changing Bar Color
-ggplot(data=dataset, aes(x=dose, y=len))+
-  geom_bar(stat = "identity", color= "blue",fill= "white", width = 0.75)
+# Changing colors
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", color = "blue", fill = "white", width = .5)
 
 # Minimal theme with blue fill color
-ggplot(data=dataset, aes(x=dose, y=len))+
-  geom_bar(stat = "identity", color= "blue",fill= "steelblue")+
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
   theme_minimal()
 
-#Bar plot with labels
-#outside bar
-ggplot(data=dataset, aes(x=dose, y=len))+
-  geom_bar(stat = "identity",fill= "steelblue")+
-  geom_text(aes(label=len), vjust =-0.5, size = 3, color= "#000000")+
+# Barplot with labels
+# Outeside bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = len), vjust = -0.5, size = 3, color = "#9F9F9F") +
   theme_minimal()
 
-#inside bar
-ggplot(data=dataset, aes(x=dose, y=len))+
-  geom_bar(stat = "identity",fill= "steelblue")+
-  geom_text(aes(label=len), vjust =1.6, size = 3, color= "white")+
+# Inside bars
+ggplot(data = dataset, aes(x = dose, y = len)) + 
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = len), vjust = 1.6, size = 3, color = "white") +
   theme_minimal()
 
-#my car data mtcars
+myCarData = mtcars
+# Barplot of counts
+# To make a barplot of counts, we will use mtcars dataset
 
-mycardata=mtcars
-
-#barplot of counts
-# To make a bar plot of counts, we will use mtcars dataset
-
-ggplot(data=mycardata, aes(x= factor(cyl)))+
-  geom_bar(stat = "count", width = 0.75, fill= "steelblue")+
+ggplot(data = myCarData, aes(x = factor(cyl))) +
+  geom_bar(stat = "count", width = 0.7, fill = "steelblue") +
   theme_minimal()
 
-#Changing barplot line colors by groups
+# Changing barplot line colors by groups
 print(dataset)
-ggplot(dataset, aes(x=dose, y=len, color=dose))+
-  geom_bar(stat="identity", fill="white")
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white")
+
+# Using custom color palettes
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") +
+  scale_color_manual(values = c("#85713a", "#c72ce6", "#f00a24"))
+
+# Using brewer color pallets
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") +
+  scale_color_brewer(palette = "Dark2")
+
+# Using grey scale
+ggplot(dataset, aes(x = dose, y = len, color = dose)) +
+  geom_bar(stat = "identity", fill = "white") +
+  scale_color_grey() +
+  theme_classic()
+
+# Changing barplot fill colors by group
+p = ggplot(dataset, aes(x = dose, y = len, fill = dose)) +
+  geom_bar(stat = "identity") + 
+  theme_minimal()
+
+p + scale_fill_manual(values = c("#85713a", "#c72ce6", "#f00a24"))
+
+p + scale_fill_brewer(palette = "Dark2")
+
+p + scale_fill_grey()
+
+# Using black outline color
+ggplot(dataset, aes(x = dose, y = len, fill = dose)) +
+  geom_bar(stat = "identity", color = "black") +
+  scale_color_manual(values = c("#85713a", "#c72ce6", "#f00a24")) +
+  theme(legend.position = "none")
+
+
