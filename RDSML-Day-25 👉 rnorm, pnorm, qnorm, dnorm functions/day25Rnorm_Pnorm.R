55c02169283027aba8ae4d7666ee2096dce8f71e
Azizul Hakim <hakim861202@gmail.com>
2025-07-28 22:24:47 +0600

    Create day25Rnorm_Pnorm.R

---
+++ RDSML-Day-25 👉 rnorm, pnorm, qnorm, dnorm functions/day25Rnorm_Pnorm.R
@@ -0,0 +1,30 @@
+
+#rnorm (randon normal distribution)
+#we want 10 value of std = closeto 1 and mean close to /= 0
+#rnorm(10)
+#x= rnorm(10)
+mean(x)
+sd(x)
+rnorm(1000)
+x=rnorm(1000)
+mean(x)
+sd(x)
+rnorm(1000, mean=0, sd=1)
+
+
+y= rnorm(10, mean=100, sd=5)
+y
+mean(y)
+
+sd(y)
+
+#pnorm 
+
+#at z =0 , then the value of probability 0.5
+
+pnorm(0)
+
+pnorm(-1.96)
+
+
+
